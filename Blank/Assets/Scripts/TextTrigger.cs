63b7de77cb3e8cd386a9f5b173857bc1c7d0072d
carolalvarez <carolalvarezmolina@gmail.com>
2022-10-29 21:45:08 +0200

    PrincipioTutorial

--- Blank/Assets/Scripts/TextTrigger.cs
+++ Blank/Assets/Scripts/TextTrigger.cs
@@ -5,13 +5,5 @@ using TMPro;
 
 public class TextTrigger : MonoBehaviour
 {
-
-    [SerializeField] private TextMeshProUGUI movimiento;
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Trigger") movimiento.text = "Utiliza A y D para moverte";
-    }
-
-
+    
 }
