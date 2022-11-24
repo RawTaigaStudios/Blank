c09ffe7bbb9e2a08adee2d5fc490b0007e50281b
Iñaki Cezon Ortega <inaki@autorizado.net>
2022-11-24 11:51:16 +0100

    HotFix

--- Blank/Assets/Scripts/CharacterController.cs
+++ Blank/Assets/Scripts/CharacterController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class CharacterController : MonoBehaviour
