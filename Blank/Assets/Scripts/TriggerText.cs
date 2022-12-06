50d452e2da3f273ebcc509caf26bd81799cbb4f3
carolalvarez <carolalvarezmolina@gmail.com>
2022-12-06 13:44:07 +0100

    fix textos

--- Blank/Assets/Scripts/TriggerText.cs
+++ Blank/Assets/Scripts/TriggerText.cs
@@ -23,8 +23,9 @@ public class TriggerText : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            Debug.Log("touching");
             texto.SetActive(true);
         }
     }
+
+    
 }
